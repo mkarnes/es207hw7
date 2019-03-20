ba0a8a5a674f703b650bf3b8899b40247f394562
stefanocasirati <stefanocasirati@gmail.com>
2019-03-20 15:38:07 -0700

    #comments in each function

--- Q10 complete address search function.R
+++ Q10 complete address search function.R
@@ -5,3 +5,7 @@
 
 complete <- length(loc$Address) - sum(str_detect(loc$Address, "^\\d") & str_detect(loc$`Zip Code`, "\\d{5}"), na.rm = T)
 complete
+
+
+# it detects if an address starts with numbers but it doesn't say that the address is incorrect if it is composed only by one number
+#the code is well written and formatted. I sudjest some comments that could be useful to understand which variables are you using 
--- Q15 daily mean function.R
+++ Q15 daily mean function.R
@@ -9,3 +9,6 @@ an_daily_mean <- function(x,y){
 }
 
 an_daily_mean(daily.site, "Merced")
+
+#the code is well written and formatted. I sudjest some comments that could be useful to understand which variables are you using 
+# the function uses global variables, I sudjest to use local variables (inside de function) that are more memory safe
